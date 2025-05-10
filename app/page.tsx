2088d8bd98cf9a091abda19574914f4407cd83f0
MonmanPsk <monman321@hotmail.com>
2025-05-10 19:39:07 +0700

    feat: create inspect page and optimize home page

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,23 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-gray-500 main-wrap bg-gradient">
-      <Image src="https://ywc20.ywc.in.th/ywc20-logo-main.webp" alt="YWC 20" width={500} height={500} />
-      <Link href={"/announcement"} className="mt-30 rounded-lg">
-        <button className="cursor-pointer bg-y20-gradient px-5 py-3 rounded-lg flex gap-2 hover:opacity-90 text-white font-semibold duration-200">
+    <main className="flex flex-col items-center justify-center h-screen main-wrap bg-gradient px-4">
+      <Image
+        src="https://ywc20.ywc.in.th/ywc20-logo-main.webp"
+        alt="YWC 20"
+        width={500}
+        height={500}
+        priority
+        className="animate-fade-in"
+      />
+      <Link href="/announcement" className="mt-30 rounded-lg">
+        <button
+          className="cursor-pointer bg-y20-gradient px-5 py-3 rounded-lg flex gap-2 hover:opacity-90 text-white font-semibold duration-200"
+          aria-label="ดูประกาศผลผู้ผ่านการคัดเลือก"
+        >
           <span className="text-xs sm:text-sm">ประกาศผลผู้ผ่านการคัดเลือก</span>
         </button>
       </Link>
-    </div>
+    </main>
   );
 }
